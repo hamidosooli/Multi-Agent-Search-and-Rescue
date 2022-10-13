a4f614c3e0e5825ca219426a2de04da98a9e777e
hamidosooli <hamid.osooli@gmail.com>
2022-10-13 18:09:32 -0400

    ant colony movement

--- agent.py
+++ agent.py
@@ -87,6 +87,61 @@ class Agent:
             elif self.action == 3:  # left
                 self.curr_Pos = [row, max(col - speed, 0)]
 
+    def get_dist(self, pos1, pos2):
+        """ takes two 2 element arrays representing positios in the grid
+            return the distance between the two positions calculated via pythagorean theorem
+        """
+        return np.sqrt((pos1[0] - pos2[0]) ** 2 + (pos1[1] - pos2[1]) ** 2)
+
+    def get_nearby_location_visits(self, grid_cells):
+        """ takes the grid of cells (represented by a 2D numpy array)
+            returns a  of the locations (as x,y tuples) which are 1 unit away
+            (ie: is UP, DOWN, LEFT, RIGHT of current agent position) together with their
+            visit count which is an integer representing the number of times the cell has been visited
+        """
+        nearby_cell_visits = list()
+        for row in range(0, len(grid_cells)):
+            for col in range(0, len(grid_cells[row, :])):
+                visit_num = grid_cells[row, col]
+                loc = [row, col]
+                if self.get_dist(loc, self.old_Pos) == 1:
+                    loc_visits = [loc, visit_num]
+                    nearby_cell_visits.append(loc_visits)
+        return nearby_cell_visits
+
+    def get_minimum_visited_cells(self, location_visits):
+        """ takes a list of tuples whose elements represent locations in the grid world together
+            with their visit counts and returns an array of locations which have the minimum number
+            of visits
+        """
+        min_visits = np.inf  # or any very large number (greater than any expected visit count)
+        min_visited_locations = []
+        # find the minimum visited number for cells corresponding with the passed locations
+        for loc_visits in location_visits:
+            times_visited = loc_visits[1]
+            if times_visited < min_visits:
+                min_visits = times_visited
+        # filter the locations corresponding with this minimum visit number
+        for loc in location_visits:
+            if loc[1] == min_visits:
+                min_visited_locations.append(loc)
+        return min_visited_locations
+
+    def ant_colony_move(self, cells_visited, idx, env_map):
+        """ takes a 2D array representing the visit count for cells in the grid world
+            and increments the current agents position toward the least visited neighboring cell
+        """
+        # increment the cell visit number
+        cells_visited[self.old_Pos[0], self.old_Pos[1]] += 1
+        if idx == (2 * self.max_VisualField + 1) ** 2:
+            nearby_location_visits = self.get_nearby_location_visits(cells_visited)
+            least_visited_locations = self.get_minimum_visited_cells(nearby_location_visits)
+            # select a random location from the least visit locations nearby
+            next_loc_ind = np.random.randint(0, len(least_visited_locations))
+            next_loc = least_visited_locations[next_loc_ind][0]
+            if env_map[next_loc[0], next_loc[1]] == 0:
+                self.curr_Pos = next_loc
+
     def update_sensation(self, index, raw_sensation, sensation_evaluate, pos2pos, net_adj_mat, adj_mat):
 
         next_sensation = [np.nan, np.nan]
